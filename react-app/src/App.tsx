f8a56aab8a5116c52cf9cbb64929990ccdf55d3f
Lauren Shu <113384782+nreualuhs@users.noreply.github.com>
2025-02-25 16:42:33 -0800

    messing with table

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,16 @@
 //import ListGroup from "./components/ListGroup";
 import ListGroupTest from "./components/ListGroupTest";
 import Title from "./components/Title";
+import TableGroup from "./components/TableGroup";
 
 function App() {
   //list of maps
   //ID = {name: "string", url: "https://blahblah", filtering content?: ["tags"]}
+
+  //goals to accomplish
+  // - add a page to the website
+  // - add rating system (add material UI?)
+  // - make it a proper table
 
   const testList = [
     {name: "Can You See Me", url: "https://canyouseeme.app/", rating: "5/5"}, 
@@ -20,21 +26,21 @@ function App() {
   }*/
 
   const handleSelectItemTest = (item: { name: string; url: string }) => {
-    //window.location.href = item.url; // Redirects the user to the URL
     window.open(item.url, "_blank");
   };
 
   return (
   <div>
     <Title title="Access Code Hub"/>
-    <ListGroupTest items={testList} heading="List of All Avaliable Tools" onSelectItem={handleSelectItemTest} />
+    <TableGroup></TableGroup>
   </div>
   );
 }
 
 export default App;
 
-/*<ListGroup items={items} heading="List of All Avaliable Tools"  onSelectItem={handleSelectItem} />
+/*<ListGroupTest items={testList} heading="List of All Avaliable Tools" onSelectItem={handleSelectItemTest} />
+<ListGroup items={items} heading="List of All Avaliable Tools"  onSelectItem={handleSelectItem} />
     <ListGroup items={filteredItems} heading="Highly Rated"  onSelectItem={handleSelectItem} />
     <ListGroup items={filteredItems} heading="JAWS compatible"  onSelectItem={handleSelectItem} />
     <ListGroup items={filteredItems} heading="NVDA add-ons"  onSelectItem={handleSelectItem} />*/
