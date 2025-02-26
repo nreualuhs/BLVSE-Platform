1e6dbad32f179c9e2a7d2c863483a498e2b490ac
Lauren Shu <113384782+nreualuhs@users.noreply.github.com>
2025-02-26 04:50:52 -0800

    added shadcn

---
+++ shad-app/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import TableGroup from './components/ui/TableGroup'
+
+
+function App() {
+  return (
+    <>
+      <h1 className= "Title">Access Code Hub</h1>
+      <TableGroup></TableGroup>
+    </>
+  )
+}
+
+export default App
---
+++ shad-app/src/components/ui/TableGroup.tsx
@@ -0,0 +1,54 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+//name, compability, description, overall rating 
+
+function TableGroup() {
+    return (
+        <>
+<Table className= "Tool-Index">
+  <TableCaption>Table of all avaliable tools</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Name</TableHead>
+      <TableHead>Compatibility</TableHead>
+      <TableHead>Description</TableHead>
+      <TableHead className="text-right">Overall Rating</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    <TableRow>
+      <TableCell className="font-medium">Can You See Me</TableCell>
+      <TableCell>NVDA, Dolphin Screen Reader, JAWS</TableCell>
+      <TableCell>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</TableCell>
+      <TableCell className="text-right">5/5</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell className="font-medium">AI Content Describer</TableCell>
+      <TableCell>NVDA add-on</TableCell>
+      <TableCell>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</TableCell>
+      <TableCell className="text-right">5/5</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell className="font-medium">IndentNav</TableCell>
+      <TableCell>NVDA add-on, Visual Studio Code</TableCell>
+      <TableCell>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</TableCell>
+      <TableCell className="text-right">5/5</TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
+</>
+    );
+}
+
+export default TableGroup;
